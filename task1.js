ca826b002b09c674d489b7c085097a5b9f74311c
DanilYaremenko <danilyaremenko@gmail.com>
2025-04-13 19:54:53 +0300

    feat: add task1 and task2 implementations

--- task1.js
+++ task1.js
@@ -1,35 +1,30 @@
-ctx.prototype.__applyStyleState = function (styleState) {
-  var keys = Object.keys(styleState),
-    i,
-    key;
-  for (i = 0; i < keys.length; i++) {
-    key = keys[i];
-    this[key] = styleState[key];
+ctx.prototype.__forEachKey = function (object, callback) {
+  const keys = Object.keys(object);
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    callback(key);
   }
+};
+
+ctx.prototype.__applyStyleState = function (styleState) {
+  this.__forEachKey(styleState, (key) => {
+    this[key] = styleState[key];
+  });
 };
 
 ctx.prototype.__setDefaultStyles = function () {
-  var keys = Object.keys(STYLES),
-    i,
-    key;
-
-  // keys of object - object selection
-  for (i = 0; i < keys.length; i++) {
-    key = keys[i];
-    this[key] = STYLES[key].canvas; // field selection
-  }
+  this.__forEachKey(STYLES, (key) => {
+    this[key] = STYLES[key].canvas;
+  });
 };
 
 ctx.prototype.__getStyleState = function () {
-  var i,
-    styleState = {},
-    keys = Object.keys(STYLES),
-    key;
+  const styleState = {};
 
-  for (i = 0; i < keys.length; i++) {
-    key = keys[i];
+  this.__forEachKey(STYLES, (key) => {
     styleState[key] = this[key];
-  }
+  });
 
   return styleState;
 };
--- task2.js
+++ task2.js
@@ -2,9 +2,18 @@ const multiply = (a, b, c) => a * b * c;
 
 const add = (a, b, c, d, e) => a + b + c + d + e;
 
-const curry = (f) => {
-  //  write your solution here
-};
+const curry =
+  (f) =>
+  (...args) => {
+    if (f.length > args.length) {
+      const g = f.bind(null, ...args);
+      return curry(g);
+    } else {
+      return f(...args);
+    }
+  };
 
-curry(add)(1)(2)(3)(4)(5) == add(1, 2, 3, 4, 5); // should be true
-curry(multiply)(1)(2)(3) == multiply(1, 2, 3); // should be true
+console.log(
+  curry(add)(1)(2)(3)(4)(5) == add(1, 2, 3, 4, 5)
+); // true
+console.log(curry(multiply)(1)(2)(3) == multiply(1, 2, 3)); // true
